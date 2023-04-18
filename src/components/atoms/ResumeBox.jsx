5d97f536a792274f9e2812cc46ecc61291fdde1b
sirMelka <melkabotale1@gmail.com>
2023-04-18 02:33:05 +0200

    feat: add notes components

--- src/components/atoms/ResumeBox.jsx
+++ src/components/atoms/ResumeBox.jsx
@@ -4,7 +4,7 @@ export default function ResumeBox() {
   return (
     <div className="resume-box">
       <h3 className="font-semibold">Moyenne</h3>
-      <p className="font-bold">notes</p>
+      <p className="font-bold">note</p>
     </div>
   );
 }
